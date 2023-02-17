79bc0c328c26f976631c8db1405e72ab14d82151
Yulany Munevar <andra_852@hotmail.com>
2023-02-16 21:47:33 -0500

    login validation and email or password

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -12,16 +12,27 @@ const LogIn = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [dataError, setDataError] = useState('');
-  const validateUser = useSelector((state) => state.products?.users);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModal = (e) => {
+    setIsOpen(!isOpen);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (Object.keys(form).length < 2) {
-      setDataError('*Este campo es requerido');
-      dispatch(loginUser(form));
-    } else {
-      dispatch(loginUser(form));
-      navigate('/home');
+    try {
+      if (Object.keys(form).length < 2) {
+        setDataError('*Este campo es requerido');
+      } else {
+        const response = await dispatch(loginUser(form));
+        if (response.payload.message === 'Invalid email or password') {
+          setIsOpen(!isOpen);
+        } else {
+          navigate('/home');
+        }
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -34,6 +45,14 @@ const LogIn = () => {
         <label htmlFor="email" className="email">Email</label>
         <input type="text" className="LogInContainer__email--input" name="email" onChange={handleChange} placeholder=" Email" />
         {dataError ? <span className="error">{dataError}</span> : null}
+        {isOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <p className="modal__text">Email o contraseña invalidos</p>
+              <button className="modal__button" type="submit" onClick={handleModal}>Cerrar</button>
+            </div>
+          </div>
+        )}
       </div>
       <div className="LogInContainer__password">
         <label htmlFor="name" className="password">Password</label>
