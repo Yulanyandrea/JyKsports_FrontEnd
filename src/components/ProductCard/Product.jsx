24d067e4e01c0226c4b37c64a6a6c18f7ba1a8d2
Yulany Munevar <andra_852@hotmail.com>
2023-02-18 15:10:14 -0500

    functions to role ADMIN or USER

--- src/components/ProductCard/Product.jsx
+++ src/components/ProductCard/Product.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './style.css';
@@ -9,27 +8,58 @@ import { productData } from '../../features/product/productSlice';
 
 const Product = ({ products }) => {
   const dispatch = useDispatch();
+  const userRole = useSelector((state) => state.products.users?.profile?.role);
+  // show buttons update
   const [show, setShow] = useState(false);
+  // show modal not allow
+  const [isOpen, setIsOpen] = useState(false);
+  // show modal to update
+  const [update, setUpdate] = useState(false);
+  // take the value for the inputs
   const { form, handleChange } = useForm({});
+
+  const handleModal = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
+  const handleUpdateModal = (e) => {
+    e.preventDefault();
+    setUpdate(!update);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setShow(true);
+    if (userRole === 'USER') {
+      setShow(false);
+      setIsOpen(!isOpen);
+    } else if (userRole === 'ADMIN') {
+      setShow(true);
+      setUpdate(!update);
+    }
   };
 
   const handleSubmitDelete = async (e) => {
-    try {
-      const response = await deleteProduct(products._id);
-      dispatch(productData());
-    } catch (error) {
-      console.error(error);
+    e.preventDefault();
+    if (userRole === 'USER') {
+      setIsOpen(!isOpen);
+    } else {
+      try {
+        const response = await deleteProduct(products._id);
+        console.log(response);
+        dispatch(productData());
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
   const handleUpdateProduct = async (e) => {
+    e.preventDefault();
     try {
       const reponseUp = await updateProduct(products._id, form);
       dispatch(productData(reponseUp));
+      setShow(false);
     } catch (error) {
       console.log(error);
     }
@@ -72,16 +102,34 @@ const Product = ({ products }) => {
       <section className="containerButtons">
         <div className="containerButtons__button--info">
           <button type="submit" className="containerButtons__buttonUpdate" onClick={handleSubmit}>Actualizar</button>
-          {show && (
-          <><input type="reference" className="containerButtons__updateRef" name="reference" onChange={handleChange} placeholder="Refencia" />
-            <input type="brand" className="containerButtons__updateBrand" name="brand" onChange={handleChange} placeholder="Marca" />
-            <input type="size" className="containerButtons__updateSize" name="size" onChange={handleChange} placeholder="Talla" />
-            <input type="amount" className="containerButtons__updateBrand" name="amount" onChange={handleChange} placeholder="Cantidad en bodega" />
-            <input type="amount" className="containerButtons__updateBrand" name="color" onChange={handleChange} placeholder="Color" />
+          {update && (
+          <div className="modal">
+            <div className="modal-content">
+              <p className="modal__text">Puedes actualizar uno o todos los campos del producto</p>
+              <img src="/images/shoes.gif" alt="" width="80" className="modal__image--shoes" />
+              <button className="modal__button" type="submit" onClick={handleUpdateModal}>Cerrar</button>
+            </div>
+          </div>
+          )}
+          { show && (
+          <><input type="text" className="containerButtons__updateRef" name="reference" onChange={handleChange} placeholder="Refencia" />
+            <input type="text" className="containerButtons__updateBrand" name="brand" onChange={handleChange} placeholder="Marca" />
+            <input type="number" className="containerButtons__updateSize" name="size" onChange={handleChange} placeholder="Talla" />
+            <input type="number" className="containerButtons__updateBrand" name="amount" onChange={handleChange} placeholder="Cantidad en bodega" />
+            <input type="text" className="containerButtons__updateBrand" name="color" onChange={handleChange} placeholder="Color" />
             <button type="submit" className="containerButtons__buttonAccept" onClick={handleUpdateProduct}>Aceptar</button>
 
           </>
 
+          )}
+          {isOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <p className="modal__text">No estas autorizado para modificar o borrar articulos</p>
+              <img src="/images/not.webp" alt="" className="modal__image" />
+              <button className="modal__button" type="submit" onClick={handleModal}>Cerrar</button>
+            </div>
+          </div>
           )}
         </div>
         <button type="submit" className="containerButtons__buttonDelete" onClick={handleSubmitDelete}>Borrar</button>
