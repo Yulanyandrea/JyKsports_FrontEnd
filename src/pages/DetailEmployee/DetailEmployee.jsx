72113e28098138a6a92957a68847421f1bd62897
Yulany Munevar <andra_852@hotmail.com>
2023-03-16 20:38:48 -0500

    navigate to detail employee page

---
+++ src/pages/DetailEmployee/DetailEmployee.jsx
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import Header from '../../components/Header/Header';
+import './style.css';
+
+const DetailEmployee = () => {
+  const { userId } = useParams();
+  const employees = useSelector((state) => state.products.usersDataBase);
+
+  return (
+    <section className="DetailEmployee">
+      <Header />
+      {
+        employees.map((employee) => {
+          return (
+            employee._id === userId ? (
+              <section className="DetailEmployee__section">
+                <label htmlFor="name" className="DetailEmployee__name">Nombre</label>
+                <label htmlFor="name" className="DetailEmployee__name--text">{employee.name}{employee.lastName}</label>
+                <label htmlFor="name" className="DetailEmployee__name">Cargo</label>
+                <label htmlFor="role" className="DetailEmployee__name--text">{employee.role}</label>
+              </section>
+            ) : null
+
+          );
+        })
+      }
+
+    </section>
+
+  );
+};
+export default DetailEmployee;
