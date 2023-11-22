1e5e4186f094278e449e01d16f7fab940d5a8979
Yulany Munevar <ddb_validator@apple.com>
2023-11-22 18:39:31 -0500

    adding changes to breadcrumb

--- src/pages/DetailEmployee/DetailEmployee.jsx
+++ src/pages/DetailEmployee/DetailEmployee.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+/* eslint-disable quotes */
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Breadcrumbs from "../../components/Breadcrumb/Breadcrumb";
 
-import Header from '../../components/Header/Header';
-import './style.css';
+import Header from "../../components/Header/Header";
+import "./style.css";
 
 const DetailEmployee = () => {
   const [edit, setEdit] = useState(false);
@@ -18,39 +20,68 @@ const DetailEmployee = () => {
   return (
     <section className="DetailEmployee">
       <Header />
-      {
-        employees.map((employee) => {
-          return (
-            employee._id === userId ? (
-              <section className="DetailEmployee__section">
-                <label htmlFor="name" className="DetailEmployee__name">Nombre</label>
-                <label htmlFor="name" className="DetailEmployee__name--text">{employee.name} {employee.lastName}</label>
-                <label htmlFor="name" className="DetailEmployee__name">Cargo</label>
-                <label htmlFor="role" className="DetailEmployee__name--text">{employee.role}</label>
-                <label htmlFor="name" className="DetailEmployee__name">Salario</label>
-                <label htmlFor="role" className="DetailEmployee__name--text">{'$ '}{employee.pay}</label>
-                <button type="submit" className="DetailEmployee__section--btn" onClick={handleEditEmployee}>Editar</button>
-                <button type="submit" className="DetailEmployee__section--btn">Eliminar empleado</button>
+      <Breadcrumbs />
+      {employees.map((employee) => {
+        return employee._id === userId ? (
+          <section className="DetailEmployee__section">
+            <label htmlFor="name" className="DetailEmployee__name">
+              Nombre
+            </label>
+            <label htmlFor="name" className="DetailEmployee__name--text">
+              {employee.name} {employee.lastName}
+            </label>
+            <label htmlFor="name" className="DetailEmployee__name">
+              Cargo
+            </label>
+            <label htmlFor="role" className="DetailEmployee__name--text">
+              {employee.role}
+            </label>
+            <label htmlFor="name" className="DetailEmployee__name">
+              Salario
+            </label>
+            <label htmlFor="role" className="DetailEmployee__name--text">
+              {"$ "}
+              {employee.pay}
+            </label>
+            <button
+              type="submit"
+              className="DetailEmployee__section--btn"
+              onClick={handleEditEmployee}
+            >
+              Editar
+            </button>
+            <button type="submit" className="DetailEmployee__section--btn">
+              Eliminar empleado
+            </button>
 
-                {
-                  edit === true ? (
-                    <>
-                      <input type="text" className="DetailEmployee__name--edit" placeholder="Nombre" />
-                      <input type="text" className="DetailEmployee__name--edit" placeholder="Apellido" />
-                      <input type="text" className="DetailEmployee__name--edit" placeholder="Cargo" />
-                      <input type="text" className="DetailEmployee__name--edit" placeholder="Salario" />
-                    </>
-                  ) : null
-                }
-              </section>
-            ) : null
-
-          );
-        })
-      }
-
+            {edit === true ? (
+              <>
+                <input
+                  type="text"
+                  className="DetailEmployee__name--edit"
+                  placeholder="Nombre"
+                />
+                <input
+                  type="text"
+                  className="DetailEmployee__name--edit"
+                  placeholder="Apellido"
+                />
+                <input
+                  type="text"
+                  className="DetailEmployee__name--edit"
+                  placeholder="Cargo"
+                />
+                <input
+                  type="text"
+                  className="DetailEmployee__name--edit"
+                  placeholder="Salario"
+                />
+              </>
+            ) : null}
+          </section>
+        ) : null;
+      })}
     </section>
-
   );
 };
 export default DetailEmployee;
